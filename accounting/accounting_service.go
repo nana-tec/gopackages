0dc4a4c9570217bbbaf22e46f1cec1a78267659a
nana-tec <129365614+nana-tec@users.noreply.github.com>
2025-11-14 02:37:55 +0300

    addition of accounting service constructor

--- accounting/accounting_service.go
+++ accounting/accounting_service.go
@@ -1,3 +1,16 @@
 package accounting
 
 // should expose an instance of accounting service
+
+import (
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func NewAccountingService(db *mongo.Database) *AccountingService {
+
+	return &AccountingService{
+		db:       db,
+		accounts: db.Collection("accounts"),
+		journals: db.Collection("journals"),
+	}
+}
