02aa756c8c338c4cf6d3e237b60101eda669cd3b
venomous-maker <morganokumu8+1@gmail.com>
2026-03-25 16:39:10 +0300

    feat: add common endpoint slugs for Create Transaction in Directline package

---
+++ Directline/constants.go
@@ -0,0 +1,9 @@
+package directline
+
+// Common bima endpoint slugs for Create Transaction
+const (
+	EndpointBusTPO      = "bustpo"
+	EndpointBusComp     = "buscomp"
+	EndpointPrivateTPO  = "privatetpo"
+	EndpointPrivateComp = "privatecomp"
+)
