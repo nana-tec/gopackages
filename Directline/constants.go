37ce1508bda700065f1a6fa8ab0f908bf22f3c98
venomous-maker <morganokumu8+1@gmail.com>
2026-04-02 12:19:01 +0300

    feat: add additional endpoint slugs for various transportation types in constants

--- Directline/constants.go
+++ Directline/constants.go
@@ -6,4 +6,35 @@ const (
 	EndpointBusComp     = "buscomp"
 	EndpointPrivateTPO  = "privatetpo"
 	EndpointPrivateComp = "privatecomp"
+
+	EndpointMatatuTPO       = "matatutpo"
+	EndpointMatatuComp      = "matatucomp"
+	EndpointTaxiTPO         = "taxitpo"
+	EndpointTaxiComp        = "taxicomp"
+	EndpointPrivateHireTPO  = "privatehiretpo"
+	EndpointPrivateHireComp = "privatehirecomp"
+
+	EndpointOwnGoodsTPO       = "owngoodstpo"
+	EndpointOwnGoodsComp      = "owngoodscomp"
+	EndpointInstitutionalTPO  = "institutionaltpo"
+	EndpointInstitutionalComp = "institutionalcomp"
+	EndpointPrimeMoverTPO     = "primemovertpo"
+	EndpointPrimeMoverComp    = "primemovercomp"
+	EndpointTrailerTPO        = "trailertpo"
+	EndpointTrailerComp       = "trailercomp"
+	EndpointTankersTPO        = "tankerstpo"
+	EndpointTankersComp       = "tankerscomp"
+
+	EndpointBikeTPO  = "biketpo"
+	EndpointBikeComp = "bikecomp"
+	EndpointBodaTPO  = "bodatpo"
+	EndpointBodaComp = "bodacomp"
+
+	EndpointGeneralCartageTPO  = "generalcartagetpo"
+	EndpointGeneralCartageComp = "generalcartagecomp"
+	EndpointTractorTPO         = "tractortpo"
+	EndpointTractorComp        = "tractorcomp"
+
+	EndpointFlammableTankersTPO  = "flammabletankerstpo"
+	EndpointFlammableTankersComp = "flammabletankerscomp"
 )
